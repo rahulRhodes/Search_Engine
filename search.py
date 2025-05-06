31ecb74c4e0de29b4ed1e913950d09956631ba63
rahulRhodes <88572309+rahulRhodes@users.noreply.github.com>
2025-05-06 07:39:35 +0530

    adding app.py file

---
+++ search.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
+from langchain.agents import initialize_agent,AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+
+
+
+
+#Arxiv and wikipedia Tools
+api_wrapper_arxiv=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=250)
+arxiv=ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
+
+
+api_wrapper_wiki=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=250)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
+
+
+search=DuckDuckGoSearchRun(name="Search")
+
+
+
+st.title(" Langchain - Chat wth search")
+"""
+In this example, we are using StreamlitCallbackhandler to display the thoughts and actions"""
+
+
+
+#sidebar for settings
+st.sidebar.title("Settings")
+api_key=st.sidebar.text_input("Enter your GROQ API key:",type="password")
+
+
+
+if "messages" not in st.session_state:
+    st.session_state['messages']=[
+        {"role":"assistant","content":"Hi, I am a chatbot who can search the web. How can I help u"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if prompt:=st.chat_input(placeholder="What is machine learning ?"):
+    st.session_state.messages.append({"role":"user","content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm=ChatGroq(groq_api_key=api_key,model="Llama3-8b-8192",streaming=True)
+    tools=[search,arxiv,wiki]
+
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_error=True)
+    with st.chat_message("assistant"):
+        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+        response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
+        st.session_state.messages.append(
+            {'role':'assistant',"content" :response}
+             )
+        st.write(response)
